d405891f3bfcfcc877fe727c41b7f3db814fddb8
BM Hasanuzzaman <47479370+BMHasanuzzaman@users.noreply.github.com>
2021-01-16 22:06:06 +0600

    creating bottom Nav

--- app/src/main/java/com/malariadetection/HubPage.java
+++ app/src/main/java/com/malariadetection/HubPage.java
@@ -1,14 +1,62 @@
 package com.malariadetection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class HubPage extends AppCompatActivity {
+
+    private static final String TAG =HubPage.class.getSimpleName() ;
+    ChipNavigationBar bottomNav;
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub_page);
+
+        bottomNav = findViewById(R.id.bottom_nav);
+        if(savedInstanceState==null){
+            bottomNav.setItemSelected(R.id.home, true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, homeFragment)
+                    .commit();
+        }
+        bottomNav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int id) {
+                Fragment fragment = null;
+
+                switch (id){
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.dashboard:
+                        fragment = new TestFragment();
+                        break;
+                    case R.id.report:
+                        fragment = new ReportFragment();
+                        break;
+                }
+
+                if(fragment !=null){
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+
+                }else{
+                    Log.e(TAG, "ERROR creating Fragment");
+                }
+            }
+        });
     }
 }
