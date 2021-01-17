12ce95da7b3a41e7be5a0a82bc5913ddfbd5d0fa
BM Hasanuzzaman <47479370+BMHasanuzzaman@users.noreply.github.com>
2021-01-17 15:05:34 +0600

    update

--- app/src/main/java/com/malariadetection/ReportFragment.java
+++ app/src/main/java/com/malariadetection/ReportFragment.java
@@ -1,5 +1,6 @@
 package com.malariadetection;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,8 @@ public class ReportFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Button b2;
 
     public ReportFragment() {
         // Required empty public constructor
@@ -58,7 +64,17 @@ public class ReportFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        View v = inflater.inflate(R.layout.fragment_report,container, false);
+        b2 = v.findViewById(R.id.button);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),Login.class));
+            }
+        });
+
+        return v;
     }
+
 }
